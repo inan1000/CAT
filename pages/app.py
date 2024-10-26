45ef23b4f9b0f7d83b2f124b6514874187ac25a6
pirite08 <jamesx0820@gmail.com>
2024-10-26 13:54:35 +0900

    ㅁㄴ

--- pages/app.py
+++ pages/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import main
-import rank
+import pages.순위 as 순위
 import cat3
 
 # 사이드바에 페이지 선택 메뉴 추가
@@ -11,7 +11,7 @@ page = st.sidebar.selectbox("페이지를 선택하세요", ["메인 페이지",
 if page == "메인 페이지":
     main.app()
 elif page == "순위표 페이지":
-    rank.app()
+    순위.app()
 elif page == "개인 미션 페이지":
     cat3.app()
 
